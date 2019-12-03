b749104b41260e9a9301022172aeef62e4f92f82
Roel <tijerinaroel@gmail.com>
2019-12-03 01:22:18 -0600

    restored to previous project version

---
+++ AquilaSoftware2.0/Pages/DrivingScreen.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AquilaSoftware2._0.Pages
+{
+   
+    public class DrivingScreenModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        private int test;
+
+        public DrivingScreenModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- AquilaSoftware2.0/Pages/SettingsScreen.cshtml.cs
+++ AquilaSoftware2.0/Pages/SettingsScreen.cshtml.cs
@@ -4,19 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace AquilaSoftware2._0.Pages
 {
-    public class IndexModel : PageModel
+    public class SettingsScreenModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
 
