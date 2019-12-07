425892c004ca914f58b583059b05b0c0f380bd2a
gator2018 <roel@Roels-MacBook-Pro.local>
2019-12-07 16:23:25 -0600

    Serial Port trial

--- AquilaSoftware2.0/Hubs/Comms.cs
+++ AquilaSoftware2.0/Hubs/Comms.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
-
+using System;
+using System.IO.Ports;
+using System.Threading;
 //Hub is the middle man communication from the server to the client side code
 namespace AquilaSoftware2._0.Hubs
 {
 
     public class Comms : Hub
     {
+        private SerialPort mySerialPort = new SerialPort("COM3");
         private IDictionary<string, int> dict = new Dictionary<string, int>();
+
+
+        public Comms()
+         {
+            mySerialPort.BaudRate = 115200;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+            mySerialPort.DtrEnable = true;
+            try
+            {
+                mySerialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                dict.Add("exeption", 4);
+            }
+        }
+
+      
+
         public async Task Request()
         {
             await Clients.All.SendAsync("Request",getValues());
@@ -19,10 +45,19 @@ namespace AquilaSoftware2._0.Hubs
 
         public string getValues()
         {
-            
-            dict.Add("rpm", 2);
-            dict.Add("speed",30);
-                
+           
+
+            if (mySerialPort.IsOpen)
+            {
+                dict.Add("rpm", 2);
+            }
+            else
+            {
+                dict.Add("speed", 30);
+
+            }
+
+
             var json = JsonConvert.SerializeObject(dict);
             return json;
         }
